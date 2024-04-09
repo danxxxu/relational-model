6ff73cdff3a3e7a924b41f6737e84389183b4aed
DanXxxxx <danxu0910@gmail.com>
2024-04-09 13:36:46 +0200

    attempt to add svg to pdf (svg not showing in pdf)

---
+++ savepdf.js
@@ -0,0 +1,19 @@
+import '../node_modules/svg2pdf.js/dist/svg2pdf.umd.min.js';
+
+document.querySelector('#savePDF').addEventListener('click', savePDF);
+
+function savePDF() {
+
+    const svgEle = document.getElementById('container');
+
+    const pdf = new jspdf.jsPDF();
+
+    pdf.svg(svgEle, {
+        x: 0,
+        y: 0,
+        width: 960,
+        height: 500
+    }).then(() => {
+        pdf.save('visualisation.pdf');
+    })
+}
